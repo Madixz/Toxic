e38cf9c0cad23dc6e624ae5c738ab0495fe76718
Exor <80299854+Madixz@users.noreply.github.com>
2021-04-29 02:27:30 +0800

    Create toxic.py

---
+++ userbot/modules/toxic.py
@@ -0,0 +1,148 @@
+from userbot import CMD_HELP, bot
+from telethon import events
+import asyncio
+
+
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 3
+
+    animation_ttl = range(0, 103)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "sinick1":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+            "`Sayang Pengen🥺` ",
+            " Udah Kebelet ",
+            " Pengen Ngewe ",
+            " Ayangg Ayokkk ",
+            " Ngeweee ",
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 103])
+
+
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 3
+
+    animation_ttl = range(0, 103)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "sinick2":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+            "`Masih Teringat` ",
+            " Di Malam Itu Kita Bercumbu",
+            " Di Bawah Sinar Rembulan ",
+            " Engkau MencumbuiKu ",
+            " Dengan Sangat Ganas ",
+            " Kau Isap Kontol Ku ",
+            " Dan Ku Jilat Memek Mu ",
+            " Skip Nanti Lu Sange",
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 103])
+
+
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 3
+
+    animation_ttl = range(0, 103)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "sinick3":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+            "`Teruntuk Kamu` ",
+            " Hay Cantik, Sudah Lama ",
+            " Perasaan Ini Ku SembunyiKan ",
+            " Tapi Sekarang ",
+            " Aku Tak Dapat Menampung Semuanya ",
+            " Semakin Aku Diam, Semakin Aku Gelisah ",
+            " Hati Ini Menginginkan Dirimu ",
+            " Mau Kah Kamu Menjadi PendampingKu?",
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 103])
+
+
+@bot.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 7
+
+    animation_ttl = range(0, 103)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "sinick4":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+            " 𝕜𝕒𝕥𝕒𝕔𝕚𝕟𝕥𝕒 ",
+            " 𝕝𝕖𝕓𝕚𝕙 𝕓𝕒𝕚𝕜 𝕕𝕚𝕓𝕖𝕟𝕔𝕚 𝕒𝕡𝕒 𝕒𝕕𝕒𝕟𝕪𝕒 𝕕𝕚𝕣𝕚𝕞𝕦 𝕕𝕒𝕣𝕚𝕡𝕒𝕕𝕒 𝕕𝕚𝕔𝕚𝕟𝕥𝕒𝕚 𝕜𝕒𝕣𝕖𝕟𝕒 𝕓𝕦𝕜𝕒𝕟 𝕕𝕚𝕣𝕚𝕞𝕦 ",
+            " 𝕔𝕚𝕟𝕥𝕒 𝕤𝕖𝕛𝕒𝕥𝕚 𝕞𝕖𝕞𝕒𝕟𝕕𝕒𝕟𝕘 𝕜𝕖𝕝𝕖𝕞𝕒𝕙𝕒𝕟 𝕝𝕒𝕝𝕦 𝕕𝕚𝕛𝕒𝕕𝕚𝕜𝕒𝕟 𝕜𝕖𝕝𝕖𝕓𝕚𝕙𝕒𝕟 𝕦𝕟𝕥𝕦𝕜 𝕤𝕖𝕝𝕒𝕝𝕦 𝕞𝕖𝕟𝕔𝕚𝕟𝕥𝕒𝕚 ",
+            " 𝕞𝕖𝕣𝕖𝕝𝕒𝕜𝕒𝕟, 𝕞𝕖𝕝𝕖𝕡𝕒𝕤𝕜𝕒𝕟 𝕓𝕦𝕜𝕒𝕟 𝕤𝕖𝕜𝕒𝕕𝕒𝕣 𝕞𝕖𝕟𝕘𝕦𝕣𝕒𝕚 𝕚𝕜𝕒𝕥𝕒𝕟. 𝕥𝕖𝕥𝕒𝕡 𝕒𝕜𝕒𝕟 𝕒𝕕𝕒 𝕤𝕚𝕤𝕒 𝕪𝕒𝕟𝕘 𝕥𝕖𝕣𝕥𝕚𝕟𝕘𝕘𝕒𝕝. 𝕤𝕖𝕝𝕖𝕤𝕒𝕚𝕜𝕒𝕟 𝕕𝕒𝕟 𝕞𝕦𝕝𝕒𝕚𝕝𝕒𝕙 𝕪𝕒𝕟𝕘 𝕓𝕒𝕣𝕦. ",
+            " 𝕔𝕚𝕟𝕥𝕒 𝕒𝕕𝕒𝕝𝕒𝕙 𝕜𝕠𝕟𝕕𝕚𝕤𝕚 𝕕𝕚 𝕞𝕒𝕟𝕒 𝕜𝕖𝕓𝕒𝕙𝕒𝕘𝕚𝕒𝕒𝕟 𝕠𝕣𝕒𝕟𝕘 𝕝𝕒𝕚𝕟 𝕤𝕒𝕟𝕘𝕒𝕥 𝕡𝕖𝕟𝕥𝕚𝕟𝕘 𝕦𝕟𝕥𝕦𝕜𝕞𝕦 𝕤𝕖𝕟𝕕𝕚𝕣𝕚 ",
+            " 𝕒𝕜𝕦 𝕞𝕖𝕟𝕔𝕚𝕟𝕥𝕒𝕚𝕞𝕦 𝕜𝕒𝕣𝕖𝕟𝕒 𝕙𝕒𝕝-𝕙𝕒𝕝 𝕘𝕖𝕝𝕒𝕡 𝕥𝕖𝕣𝕥𝕖𝕟𝕥𝕦 𝕙𝕒𝕣𝕦𝕤 𝕕𝕚𝕔𝕚𝕟𝕥𝕒𝕚, 𝕤𝕖𝕔𝕒𝕣𝕒 𝕣𝕒𝕙𝕒𝕤𝕚𝕒, 𝕒𝕟𝕥𝕒𝕣𝕒 𝕓𝕒𝕪𝕒𝕟𝕘𝕒𝕟 𝕕𝕒𝕟 𝕛𝕚𝕨𝕒 ",
+            " 𝕥 𝕖 𝕣 𝕚 𝕞 𝕒  𝕜 𝕒 𝕤 𝕚 𝕙 ",
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 103])
+
+
+CMD_HELP.update({
+    "toxic":
+    "`.sinick1`\
+    \nUsage: pengen.\
+    \n\n`.sinick2`\
+    \nUsage: sange."
+})

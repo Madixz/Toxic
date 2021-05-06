790dd1ccd927df9af3865bb55f978fced1ced6c5
Exor <80299854+Madixz@users.noreply.github.com>
2021-05-06 12:47:19 +0800

    Update toxic2.py

--- userbot/modules/toxic2.py
+++ userbot/modules/toxic2.py
@@ -28,7 +28,7 @@ opener.addheaders = [('User-agent', useragent)]
 
 
 
-@register(outgoing=True, pattern="^.CHB$")
+@register(outgoing=True, pattern="^.bkp$")
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
@@ -42,28 +42,10 @@ async def repo_is_here(wannasee):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        f"⚜️ ┗┓ ** R U L E S ** ┏┛ ⚜️ \n\n"
-        f"** ✥ Wajib Follow Ig Di Bawah **\n\n"
-        f"** ✥ Jika sudah follow ig di bawah bisa langsung pc/dm(jika tidak memilili tele) ke akun yang bersangkutan untuk follow back. **\n\n"
-        f"** ✥ Jika dalam kurun waktu 1 hari tidak difollow back bisa melaporkan ke admin dan anda bisa unfoll ig bersangkutan. **\n\n"
-        f"** ✥ Jika mau mendaftar agar ig nya ada didaftar ig bisa pc ke [** Admin ](https://t.me/Bot_Sinick) **\n\n"
-        f"⚜️ ┗┓ ** DAFTAR IG  ┏┛ ⚜️ \n\n"
-        f"✦҈͜͡➳  Bot_Sinick   : [ Instagram ](https://instagram.com/bot_sinick?igshid=16p9mzk9kne8v) || [ Tele ](https://t.me/Bot_Sinick) \n"
-        f"✦҈͜͡➳  Jess         : [ Instagram ](https://instagram.com/jeve_jess?igshid=a36x6yczxix6) \n"
-        f"✦҈͜͡➳  Ayu          : [ Instagram ](https://instagram.com/_ayy614?igshid=pzm0rejkawtz) \n"
+        f"✦҈͜͡➳ ** Link Mutualan Ig :** [ Daftar Ig ](https://telegra.ph/Daftar-Ig-05-01-2)\n"
+        f"✦҈͜͡➳ **     :** [-]( )\n"
+        f"✦҈͜͡➳ **     :** [-]( )\n"
     )
 
 
 
-
-CMD_HELP.update({
-    "jngn buka nnti eror":
-    "`.okp`\
-\nUsage: Jangan di coba kalo tidak mau eror.\
-\n\n`.okp2`\
-\nUsage: Jangan di coba kalo tidak mau eror.\
-\n\n`.okp3`\
-\nUsage: Jangan di coba kalo tidak mau eror.\
-\n\n`.viral`\
-\nUsage: Jangan di coba kalo tidak mau eror."
-})

efed726110a67fff7a481e9cd3d899976b6314ba
Exor <80299854+Madixz@users.noreply.github.com>
2021-05-06 11:13:13 +0800

    Update toxic2.py

--- userbot/modules/toxic2.py
+++ userbot/modules/toxic2.py
@@ -1,22 +1,40 @@
-from platform import uname
-from userbot import ALIVE_NAME, CMD_HELP
+""" Userbot module for other small commands. """
+
+from random import randint
+from time import sleep
+from os import execl
+import asyncio
+import sys
+import os
+import io
+import sys
+from userbot import ALIVE_NAME, BOTLOG, BOTLOG_CHATID, CMD_HELP, bot
 from userbot.events import register
+from userbot.utils import time_formatter
+import urllib
+import requests
+from bs4 import BeautifulSoup
+import re
+from PIL import Image
+
 
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
-
-@register(outgoing=True, pattern='^.CHB(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("https://t.me/joinchat/VYezAmeu_YM5NTQ9")
+opener = urllib.request.build_opener()
+useragent = 'Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.70 Mobile Safari/537.36'
+opener.addheaders = [('User-agent', useragent)]
 
 
-@register(outgoing=True, pattern='^.viral(?: |$)(.*)')
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    await typew.edit("https://drive.google.com/file/d/1GX0OuXdueKUITH1gRGgi_AxNaJ4LsGD9/view?usp=drivesdk")
+
+@register(outgoing=True, pattern="^.repo$")
+async def repo_is_here(wannasee):
+    """ For .repo command, just returns the repo URL. """
+    await wannasee.edit(
+        " \n[┣━•━─[ Bokep ]─━•━┫](https://t.me/joinchat/VYezAmeu_YM5NTQ9) \n      \n"
+    )
+
 
 
 CMD_HELP.update({

becfbc1f6fdf5dbc1f362ac8fcfb15093f508e95
Exor <80299854+Madixz@users.noreply.github.com>
2021-05-20 02:59:08 +0800

    Update toxic.py

--- userbot/modules/toxic.py
+++ userbot/modules/toxic.py
@@ -123,14 +123,18 @@ async def _(event):
         await event.edit(input_str)
 
         animation_chars = [
-            " 𝕜𝕒𝕥𝕒𝕔𝕚𝕟𝕥𝕒 ",
-            " 𝕝𝕖𝕓𝕚𝕙 𝕓𝕒𝕚𝕜 𝕕𝕚𝕓𝕖𝕟𝕔𝕚 𝕒𝕡𝕒 𝕒𝕕𝕒𝕟𝕪𝕒 𝕕𝕚𝕣𝕚𝕞𝕦 𝕕𝕒𝕣𝕚𝕡𝕒𝕕𝕒 𝕕𝕚𝕔𝕚𝕟𝕥𝕒𝕚 𝕜𝕒𝕣𝕖𝕟𝕒 𝕓𝕦𝕜𝕒𝕟 𝕕𝕚𝕣𝕚𝕞𝕦 ",
-            " 𝕔𝕚𝕟𝕥𝕒 𝕤𝕖𝕛𝕒𝕥𝕚 𝕞𝕖𝕞𝕒𝕟𝕕𝕒𝕟𝕘 𝕜𝕖𝕝𝕖𝕞𝕒𝕙𝕒𝕟 𝕝𝕒𝕝𝕦 𝕕𝕚𝕛𝕒𝕕𝕚𝕜𝕒𝕟 𝕜𝕖𝕝𝕖𝕓𝕚𝕙𝕒𝕟 𝕦𝕟𝕥𝕦𝕜 𝕤𝕖𝕝𝕒𝕝𝕦 𝕞𝕖𝕟𝕔𝕚𝕟𝕥𝕒𝕚 ",
-            " 𝕞𝕖𝕣𝕖𝕝𝕒𝕜𝕒𝕟, 𝕞𝕖𝕝𝕖𝕡𝕒𝕤𝕜𝕒𝕟 𝕓𝕦𝕜𝕒𝕟 𝕤𝕖𝕜𝕒𝕕𝕒𝕣 𝕞𝕖𝕟𝕘𝕦𝕣𝕒𝕚 𝕚𝕜𝕒𝕥𝕒𝕟. 𝕥𝕖𝕥𝕒𝕡 𝕒𝕜𝕒𝕟 𝕒𝕕𝕒 𝕤𝕚𝕤𝕒 𝕪𝕒𝕟𝕘 𝕥𝕖𝕣𝕥𝕚𝕟𝕘𝕘𝕒𝕝. 𝕤𝕖𝕝𝕖𝕤𝕒𝕚𝕜𝕒𝕟 𝕕𝕒𝕟 𝕞𝕦𝕝𝕒𝕚𝕝𝕒𝕙 𝕪𝕒𝕟𝕘 𝕓𝕒𝕣𝕦. ",
-            " 𝕔𝕚𝕟𝕥𝕒 𝕒𝕕𝕒𝕝𝕒𝕙 𝕜𝕠𝕟𝕕𝕚𝕤𝕚 𝕕𝕚 𝕞𝕒𝕟𝕒 𝕜𝕖𝕓𝕒𝕙𝕒𝕘𝕚𝕒𝕒𝕟 𝕠𝕣𝕒𝕟𝕘 𝕝𝕒𝕚𝕟 𝕤𝕒𝕟𝕘𝕒𝕥 𝕡𝕖𝕟𝕥𝕚𝕟𝕘 𝕦𝕟𝕥𝕦𝕜𝕞𝕦 𝕤𝕖𝕟𝕕𝕚𝕣𝕚 ",
-            " 𝕒𝕜𝕦 𝕞𝕖𝕟𝕔𝕚𝕟𝕥𝕒𝕚𝕞𝕦 𝕜𝕒𝕣𝕖𝕟𝕒 𝕙𝕒𝕝-𝕙𝕒𝕝 𝕘𝕖𝕝𝕒𝕡 𝕥𝕖𝕣𝕥𝕖𝕟𝕥𝕦 𝕙𝕒𝕣𝕦𝕤 𝕕𝕚𝕔𝕚𝕟𝕥𝕒𝕚, 𝕤𝕖𝕔𝕒𝕣𝕒 𝕣𝕒𝕙𝕒𝕤𝕚𝕒, 𝕒𝕟𝕥𝕒𝕣𝕒 𝕓𝕒𝕪𝕒𝕟𝕘𝕒𝕟 𝕕𝕒𝕟 𝕛𝕚𝕨𝕒 ",
-            " 𝕥 𝕖 𝕣 𝕚 𝕞 𝕒  𝕜 𝕒 𝕤 𝕚 𝕙 ",
-        ]
+            "`Connecting To Hacked Private Server...`",
+            "`Target Selected.`",
+            "`Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `\n`Looking for telegram data...`\nETA: 0m, 20s",
+            "`Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `\n`Found folder sd/telegram...`\n`Looking for telegram data : ✅`\nETA: 0m, 10s",
+            "`Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `\n`Searching for databases....`\n`Looking for telegram data : ✅`\n`Found folder sd/telegram : ✅ `\nETA: 0m, 15s",    
+            "`Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒`\n`Found msgstore.db.crypt12...`\n`Looking for telegram data : ✅`\n`Found folder sd/telegram : ✅ `\n`Searching for databases : ✅ `\nETA: 0m, 09s",
+            "`Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `\n`Trying to Decrypt...`\n`Looking for telegram data : ✅`\n`Found folder sd/telegram : ✅\n`Searching for databases : ✅`\n`Found msgstore.db.crypt12 :  ✅`\n`⚠️ error occurred ..`\nETA: 0m, 25s",
+            "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `\n`Decryption successful!`\n`Looking for telegram data : ✅`\n`⚠️ error occurred ..`\n`Found folder sd/telegram : ✅`\n`⚠️ error occurred ..`\n`Searching for databases : ✅`\n`Found msgstore.db.crypt12 :  ✅`\nETA: 0m, 25s",
+            "`Hacking... 84%\n█████████████████████▒▒▒▒ `\n`Scanning file...`\n`Looking for telegram data : ✅`\n`⚠️ error occurred ..`\n`Found folder sd/telegram : ✅`\n`⚠️ error occurred ..`\n`⚠️ error occurred ..`\n`Searching for databases : ✅`\n`Found msgstore.db.crypt12 :  ✅`\n`Scanning file :  ✅`\nETA: 0m, 16s",
+            "`Hacking... 100%\n` 98% HACKED`",
+            "`Targeted Account Hacked...`\n\n`_______________________`\n`result ... :)`\n\n`Chatlist : ✅`\n`Calls : ✅`\n`groups : ✅`\n `Contacts : ✅`\n`Channel : ✅`\n`Deleted Messages : ❌`\n`Edited Messages : ❌`\n`All API Tokens : ✅`\n\n`Pay 99999$ to my boss.... `Or send your girlfriend's number To Remove This Hack`"
+         ]
 
         for i in animation_ttl:
 
@@ -192,7 +196,6 @@ async def _(event):
             "`Hacking... 84%\n█████████████████████▒▒▒▒ `\n`Scanning file...`\n`Looking for telegram data : ✅`\n`⚠️ error occurred ..`\n`Found folder sd/telegram : ✅`\n`⚠️ error occurred ..`\n`⚠️ error occurred ..`\n`Searching for databases : ✅`\n`Found msgstore.db.crypt12 :  ✅`\n`Scanning file :  ✅`\nETA: 0m, 16s",
             "`Hacking... 100%\n` 98% HACKED`",
             "`Targeted Account Hacked...`\n\n`_______________________`\n`result ... :)`\n\n`Chatlist : ✅`\n`Calls : ✅`\n`groups : ✅`\n `Contacts : ✅`\n`Channel : ✅`\n`Deleted Messages : ❌`\n`Edited Messages : ❌`\n`All API Tokens : ✅`\n\n`Pay 99999$ to my boss.... `Or send your girlfriend's number To Remove This Hack`"
-           
   ]
 
         for i in animation_ttl:

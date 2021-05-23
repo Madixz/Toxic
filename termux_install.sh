23921190992a5a4dd552783e21f9ee4ed1d18790
Exor <80299854+Madixz@users.noreply.github.com>
2021-05-23 15:26:50 +0800

    Update termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -34,8 +34,8 @@ clear
 pkg update && pkg upgrade -y
 pkg install clang curl git libcrypt libffi libiconv libjpeg* libjpeg-turbo libwebp libxml2 libxslt make ndk-sysroot openssl postgresql python readline wget zlib -y
 
-git clone https://github.com/mrismanaziz/Man-Userbot.git
-cd Man-UserBot
+git clone https://github.com/Madixz/Toxic.git
+cd Sinick
 
 pip install --upgrade pip setuptools
 pip install -r requirements.txt

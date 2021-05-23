d8c16d19f48227ce2a2ef575ead4c5b1d46a4dde
Exor <80299854+Madixz@users.noreply.github.com>
2021-05-23 14:49:46 +0800

    Update helper.py

--- userbot/modules/helper.py
+++ userbot/modules/helper.py
@@ -11,9 +11,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def usit(e):
     await e.edit(
         f"**Hai {DEFAULTUSER} Kalo Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/mrismanaziz)"
-        "\n[Repo](https://github.com/mrismanaziz/Man-Userbot)"
-        "\n[Instagram](Instagram.com/mrismanaziz_)"
+        "\n[Telegram](t.me/Bot_Sinick)"
+        "\n[Repo](https://github.com/Madixz/Toxic)"
+        "\n[Instagram](Instagram.com/Bot_Sinick)"
     )
 
 
@@ -21,7 +21,7 @@ async def usit(e):
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/mrismanaziz/Man-Userbot/Man-Userbot/varshelper.txt)"
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/Madika/Toxic/Sinick/varshelper.txt)"
     )
 
 
@@ -29,13 +29,9 @@ CMD_HELP.update(
     {
         "helper": "**Plugin : **`helper`\
         \n\n  •  **Syntax :** `.ihelp`\
-        \n  •  **Function : **Bantuan Untuk Man-Userbot.\
+        \n  •  **Function : **Bantuan Untuk Sinick.\
         \n\n  •  **Syntax :** `.listvar`\
         \n  •  **Function : **Melihat Daftar Vars.\
-        \n\n  •  **Syntax :** `.repo`\
-        \n  •  **Function : **Melihat Repository Man-Userbot.\
-        \n\n  •  **Syntax :** `.string`\
-        \n  •  **Function : **Link untuk mengambil String Man-Userbot.\
-    "
+        "
     }
 )

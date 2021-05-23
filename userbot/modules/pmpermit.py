5968171a4cae3e563dcaea74a9a285e1aca5e99a
Exor <80299854+Madixz@users.noreply.github.com>
2021-05-23 18:08:08 +0800

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -30,7 +30,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 DEF_UNAPPROVED_MSG = (
     f"✘ WARNING ✘ \n\n"
 
-    f"Jangan Spam 8x Chat Bakal Di Blok Otomatis Oleh Saya { Penjaga Room Chat } Tunggu Sampai Bos Besar {DEFAULTUSER} Menyetujui, \n\n"
+    f"Jangan Spam 8x Chat Bakal Di Blok Otomatis Oleh Saya ( Penjaga Room Chat ) Tunggu Sampai Bos Besar {DEFAULTUSER} Menyetujui, \n\n"
 
     f"Kalau Penting Bisa Di Jelaskan Kepentingannya. \n\n"
 
